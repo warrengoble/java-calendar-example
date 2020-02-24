0826c7125d1858342541d65d865e9727ba694592
Warren Goble <warrengoble@users.noreply.github.com>
2020-02-24 01:04:52 -0700

    Added protection level and getters

--- src/main/java/com/example/calendar/Event.java
+++ src/main/java/com/example/calendar/Event.java
@@ -1,14 +1,15 @@
 package com.example.calendar;
 
-import java.time.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.UUID;
 
 public class Event {
-    UUID id;
-    LocalDate date;
-    LocalTime startTime;
-    LocalTime endTime;
-    String description;
+    private UUID id;
+    private LocalDate date;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private String description;
 
     public Event(LocalDate date, LocalTime startTime, LocalTime endTime, String description) {
         this.id = UUID.randomUUID();
@@ -16,5 +17,25 @@ public class Event {
         this.startTime = startTime;
         this.endTime = endTime;
         this.description = description;
+    }
+
+    public UUID getId() {
+        return this.id;
+    }
+
+    public LocalDate getDate() {
+        return this.date;
+    }
+
+    public LocalTime getStartTime() {
+        return this.startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return this.endTime;
+    }
+
+    public String getDescription() {
+        return this.description;
     }
 }
