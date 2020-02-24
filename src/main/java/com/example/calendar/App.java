fa21790102b99f3f6c006289758a8ba0e32cf765
Warren Goble <warrengoble@users.noreply.github.com>
2020-02-24 01:15:40 -0700

    Updated synthetic App.java calls

--- src/main/java/com/example/calendar/App.java
+++ src/main/java/com/example/calendar/App.java
@@ -5,25 +5,28 @@ import java.time.*;
 public class App {
     public static void main(String[] args) {
         System.out.println("Calendar example");
-        CalendarExample cal = new CalendarExample();
+        Calendar cal = new Calendar();
 
-        Event firstEvent = cal.addEvent(LocalDate.now().minusDays(1), LocalTime.of(12, 15, 0), LocalTime.of(12, 17, 0),
-                "Test Event 1");
+        cal.addEvent(LocalDate.now().minusDays(1), LocalTime.of(12, 15, 0), LocalTime.of(12, 17, 0),
+                "One day before");
 
-        Event secondEvent = cal.addEvent(LocalDate.now(), LocalTime.of(1, 0, 0), LocalTime.of(12, 17, 20),
-                "Test Event 2");
+        cal.addEvent(LocalDate.now(), LocalTime.now().plusHours(1), LocalTime.now().plusHours(2),
+                "An hour later");
 
-        Event thirdEvent = cal.addEvent(LocalDate.now().plusDays(1), LocalTime.of(1, 0, 0), LocalTime.of(16, 0, 30),
-                "Test Event 3");
+        cal.addEvent(LocalDate.now().plusDays(1), LocalTime.of(1, 0, 0), LocalTime.of(16, 0, 30),
+                "A day later");
 
-        Event forthEvent = cal.addEvent(LocalDate.now().plusDays(2), LocalTime.of(9, 0, 0), LocalTime.of(12, 15, 15),
-                "Test Event 4");
+        cal.addEvent(LocalDate.now().plusDays(2), LocalTime.of(9, 0, 0), LocalTime.of(12, 15, 15),
+                "Five days later");
 
-        Event fifthEvent = cal.addEvent(LocalDate.now().plusDays(6), LocalTime.of(8, 0, 0), LocalTime.of(10, 15, 0),
-                "Test Event 4");
+        cal.addEvent(LocalDate.now().plusDays(6), LocalTime.of(8, 0, 0), LocalTime.of(10, 15, 0),
+                "Six days later");
 
-        Event aheadOneWeekEvent = cal.addEvent(LocalDate.now().plusWeeks(1), LocalTime.of(16, 0, 0),
-                LocalTime.of(20, 0, 0), "Test Event 4");
+        cal.addEvent(LocalDate.now().plusDays(7), LocalTime.of(16, 0, 0),
+                LocalTime.of(20, 0, 0), "Seven days later");
+
+        cal.addEvent(LocalDate.now().plusWeeks(2), LocalTime.of(16, 0, 0),
+                LocalTime.of(20, 0, 0), "Two weeks later");
 
         cal.printRemainingAgendaForTheWeek();
     }
