409ba0833aa9adaf13fe760e41e030e6c674967a
Warren Goble <warrengoble@users.noreply.github.com>
2020-02-24 01:15:56 -0700

    Added initial tests

--- src/test/java/com/example/calendar/AppTest.java
+++ src/test/java/com/example/calendar/AppTest.java
@@ -1,16 +1,78 @@
 package com.example.calendar;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 import org.junit.Test;
 
 public class AppTest {
     @Test
     public void addEvent() {
-        assertTrue(true);
+        Calendar cal = new Calendar();
+        
+        LocalDate testDate = LocalDate.now();
+        LocalTime testStartTime = LocalTime.now();
+        LocalTime testEndTime = LocalTime.now().plusHours(6);
+        String testDescription = "Test addEvent";
+
+        Event retEvent = cal.addEvent(
+            testDate,
+            testStartTime,
+            testEndTime,
+            testDescription
+        );
+
+        assertTrue(cal.getEvents().contains(retEvent));
     }
 
+    @Test
     public void removeEvent() {
-        assertTrue(true);
+        Calendar cal = new Calendar();
+        
+        LocalDate testDate = LocalDate.now();
+        LocalTime testStartTime = LocalTime.now();
+        LocalTime testEndTime = LocalTime.now().plusHours(6);
+        String testDescription = "Test removeEvent";
+
+        Event retEvent = cal.addEvent(
+            testDate,
+            testStartTime,
+            testEndTime,
+            testDescription
+        );
+
+        cal.removeEvent(retEvent);
+
+        assertFalse(cal.getEvents().contains(retEvent));
+    }
+
+    @Test
+    public void removeEventById() {
+        Calendar cal = new Calendar();
+        
+        LocalDate testDate = LocalDate.now();
+        LocalTime testStartTime = LocalTime.now();
+        LocalTime testEndTime = LocalTime.now().plusHours(6);
+        String testDescription = "Test removeEventById";
+
+        Event retEvent = cal.addEvent(
+            testDate,
+            testStartTime,
+            testEndTime,
+            testDescription
+        );
+
+        cal.removeEventById(retEvent.getId());
+
+        assertFalse(cal.getEvents().contains(retEvent));
+    }
+
+    public void testEndOfWeek() {
+        //
     }
 }
